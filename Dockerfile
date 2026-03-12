ce84811334728c739e64cb10f139a4c8b9a617a8
Christophe Eymard <ceymard@pm.me>
2026-03-12 09:50:46 +0100

    postgres 18 with envvar, pg_cron, postgis, http and pg_jsonschema

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,11 @@
-FROM postgres:17.4-alpine
+FROM postgres:18.3-alpine AS extension-builder
 
 COPY ./root /
-RUN apk update && apk add make clang19 build-base git curl-dev perl libxml2-dev geos-dev proj-dev protobuf-c-dev gdal-dev json-c-dev llvm19 postgresql-plpython3 libcurl ca-certificates py3-pip libxml2 geos proj protobuf-c gdal json-c
+RUN apk add --no-cache make clang19 clang19-libclang build-base git curl-dev perl libxml2-dev geos-dev proj-dev protobuf-c-dev gdal-dev json-c-dev llvm19 postgresql-plpython3 libcurl ca-certificates py3-pip libxml2 geos proj protobuf-c gdal json-c rust cargo flex bison readline-dev rustfmt
 RUN ash /install.sh
+
+FROM postgres:18.3-alpine
+
+RUN apk add --no-cache geos proj protobuf-c json-c
+COPY --from=extension-builder /usr/local/share/postgresql/extension /usr/local/share/postgresql/extension
+COPY --from=extension-builder /usr/local/lib/postgresql /usr/local/lib/postgresql
--- root/install.sh
+++ root/install.sh
@@ -2,10 +2,11 @@
 
 set -e
 
-PG_CRON_VERSION="1.6.5"
-PGSQL_HTTP_VERSION="1.5.0"
-POSTGIS_VERSION="3.5.1"
-ENVVAR_VERSION="1.0.0"
+PG_CRON_VERSION="1.6.7"
+PGSQL_HTTP_VERSION="1.7.0"
+POSTGIS_VERSION="3.6.2"
+ENVVAR_VERSION="1.0.1"
+PG_JSONSCHEMA_VERSION="0.3.4"
 
 cd /usr/local/share/postgresql/extension/
 ln -sf /usr/share/postgresql/extension/plpython3u* .
@@ -33,13 +34,20 @@ cd pg_cron
 sed -i 's/-Werror //g' Makefile
 make && make install
 
+cd /build
 wget https://download.osgeo.org/postgis/source/postgis-$POSTGIS_VERSION.tar.gz
 tar xf postgis-$POSTGIS_VERSION.tar.gz
 cd postgis-$POSTGIS_VERSION
 ./configure && make && make install
 
+cd /build
+wget https://github.com/supabase/pg_jsonschema/archive/refs/tags/v${PG_JSONSCHEMA_VERSION}.tar.gz
+tar xf v$PG_JSONSCHEMA_VERSION.tar.gz
+cd pg_jsonschema-$PG_JSONSCHEMA_VERSION
 
-cd /
-rm -rf /build
-apk del build-base clang19 llvm19 git curl-dev perl libxml2-dev geos-dev proj-dev protobuf-c-dev gdal-dev json-c-dev make
-rm -f /var/cache/apk/*
+mkdir /pgrx
+export PGRX_HOME=/pgrx
+cargo install cargo-pgrx --version 0.16.1 --locked
+cargo pgrx init --pg18 $(which pg_config)
+
+cargo pgrx install
